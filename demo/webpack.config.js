bfd97e8a154f5ac717575870028c1548ac7a3e53
Fabian Esser <fabian.esser@real-digital-agency.de>
2019-12-12 15:54:29 +0100

    [TASK] init for demo styling

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
-
 module.exports = {
-  entry: ['./src/expand.js', './src/expand.scss'],
+  mode: 'development',
+  entry: ['./demo.js'],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'expand.min.js',
-    publicPath: '/dist/',
+    path: path.join(__dirname, 'assets'),
+    filename: 'demo.min.js',
+    publicPath: '/assets',
     library: 'Expand',
     libraryTarget: 'umd',
     umdNamedDefine: true
@@ -17,21 +16,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env'],
-          plugins: ['babel-plugin-add-module-exports']
-        }
-      },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
@@ -58,7 +42,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new UglifyJsPlugin(),
     new UnminifiedWebpackPlugin()
   ]
 };
